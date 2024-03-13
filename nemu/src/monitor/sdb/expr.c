4f2526fb9354fddf38622db301867b4e9b570916
tracer-ysyx <tracer@ysyx.org>
2024-03-13 12:54:04 +0800

    > compile NEMU NOT_ALLOCATED_YET Letong Han Linux dfpmts-kubuntu 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:54:04 up 44 min,  2 users,  load average: 0.24, 0.58, 0.59

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -140,12 +140,12 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();
   for (int i = 0; i < nr_token; ++i) {
-    printf("%8d", i);
+    printf("%10d", i);
   }
   puts("");
 
   for (int i = 0; i < nr_token; ++i) {
-    printf("%8s", tokens[i].str);
+    printf("%10s", tokens[i].str);
   }
   puts("");
 
