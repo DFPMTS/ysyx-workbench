4a6540ac4c15d75dcc846465641effbd2615a0a3
Letong Han <hanlt@shanghaitech.edu.cn>
2024-03-15 13:09:08 +0800

    [feat] setting TEST_EXPR now makes nemu a expr() checker

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <common.h>
+#include <stdio.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
@@ -21,7 +22,10 @@ void engine_start();
 int is_exit_status_bad();
 word_t expr(char *e, bool *success);
 
+static char buf [65536+10];
+
 int main(int argc, char *argv[]) {
+  char *test_expr_path;
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -29,12 +33,27 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  /* Start engine. */
-  // engine_start();
-  bool success;
-  char *s = "1+2+(3/3)+5*1-4";
-  printf("expr value: %u\n",expr(s, &success));
-  Assert(success,"Token recognition failed.");
+  if ((test_expr_path = getenv("TEST_EXPR"))) {
+    // format: [ref]  [expr]
+    uint32_t ref;
+    Log("Reading input from: %s\n",test_expr_path);
+    FILE *test_input = fopen(test_expr_path, "r");
+    Assert(test_input, "Failed to open file");
 
-  return is_exit_status_bad();
+    while(true){
+      if (fscanf(test_input, "%u", &ref) == EOF || !fgets(buf, 65536, test_input))
+        break;
+      bool success;
+      uint32_t expr_val = expr(buf, &success);
+      Log("expr value: %u\n", expr_val);
+      Assert(success, "Eval failed.");
+      Assert(ref == expr_val, "Wrong answer");
+    }
+  } else {
+
+    /* Start engine. */
+    engine_start();
+
+    return is_exit_status_bad();
+  }
 }
