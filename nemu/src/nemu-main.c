278d62c2175d93a95e28ca1c498a2b47cd0f1570
tracer-ysyx <tracer@ysyx.org>
2024-03-15 12:26:47 +0800

    > compile NEMU NOT_ALLOCATED_YET Letong Han Linux dfpmts-kubuntu 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:26:47 up  4:28,  2 users,  load average: 1.49, 0.74, 0.60

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -39,13 +39,16 @@ int main(int argc, char *argv[]) {
     Log("Reading input from: %s\n",test_expr_path);
     FILE *test_input = fopen(test_expr_path, "r");
     Assert(test_input, "Failed to open file");
-    Assert(fscanf(test_input, "%u", &ref), "Read reference output failed");
-    Assert(fgets(buf, 65536, test_input), "Read expr failed");    
-    bool success;
-    uint32_t expr_val = expr(buf, &success);
-    Log("expr value: %u\n", expr_val);
-    Assert(success, "Eval failed.");
-    Assert(ref == expr_val, "Wrong answer");
+
+    while(true){
+      if (fscanf(test_input, "%u", &ref) == EOF || !fgets(buf, 65536, test_input))
+        break;
+      bool success;
+      uint32_t expr_val = expr(buf, &success);
+      Log("expr value: %u\n", expr_val);
+      Assert(success, "Eval failed.");
+      Assert(ref == expr_val, "Wrong answer");
+    }
   } else {
 
     /* Start engine. */
