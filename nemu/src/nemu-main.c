354d1e1924425260b0f11289cfc11a6da9cd6338
tracer-ysyx <tracer@ysyx.org>
2024-03-15 11:55:42 +0800

    > compile NEMU NOT_ALLOCATED_YET Letong Han Linux dfpmts-kubuntu 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:55:42 up  3:57,  2 users,  load average: 0.49, 0.71, 0.57

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,7 +30,6 @@ int main(int argc, char *argv[]) {
     char *buf = malloc(65536);
     printf("%s\n",test_expr_path);
     FILE *test_input = fopen(test_expr_path, "r");
-
     Assert(fscanf(test_input, "%u", &ref), "Read reference output failed");
     Assert(fgets(buf, 65536, test_input), "Read expr failed");
     bool success;
