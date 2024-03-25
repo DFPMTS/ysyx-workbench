a1049ba3f1752a2f6527e1172033f40b981889db
Letong Han <hanlt@shanghaitech.edu.cn>
2024-03-25 21:34:18 +0800

    [feat] sprintf & vsprintf support %d, %s, %%

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,65 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char* sprintf_int(char *out, int x) {
+  char a[20];
+  size_t i = 0;
+
+  while (x) {
+    a[i++] = (x % 10) + '0';
+    x /= 10;
+  }
+  if (!i) {
+    a[i++] = '0';
+  }
+  while (i) {
+    *out++ = a[--i];
+  }
+  return out;
+}
+
+static char *sprintf_str(char *out, char *s) {
+  while (*s != '\0') {
+    *out++ = *s++;
+  }
+  return out;
+}
+
+// %[$][flags][width][.precision][length modifier]conversion
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int printed = 0;
+  while (*fmt != '\0') {
+    if (*fmt == '%') {    
+      char c = *(++fmt);
+      if (c == 'd') {
+        ++printed;
+        out = sprintf_int(out, va_arg(ap, int));
+      } else if (c == 's') {
+        ++printed;
+        out = sprintf_str(out, va_arg(ap, char *));
+      } else if (c == '%') {
+        *out++ = '%';
+      }
+    } else {
+      *out++ = *fmt;
+    }
+    fmt++;
+  }
+  *out = '\0';
+  return printed;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args,fmt);
+  int ret_val = vsprintf(out, fmt, args);
+  va_end(args);
+
+  return ret_val;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
