04e4083d6d1b6f9a9b23ce69a9caa6a416088f42
Letong Han <hanlt@shanghaitech.edu.cn>
2024-03-31 20:16:34 +0800

    [feat] simple malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -2,6 +2,10 @@
 #include <klib.h>
 #include <klib-macros.h>
 
+// for now (RV32) the alignment is 8-byte
+#define ALIGN 8
+#define ALIGN_MASK 0x7
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
@@ -33,10 +37,26 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   panic("Not implemented");
+// #endif
+  static uint32_t addr = -1;
+  // avoid touching _heap_start directly
+  if (addr == -1) {
+    addr = (uint32_t)heap.start;
+  }
+
+  // move to next 8-byte boundry
+  uint32_t ptr = (addr + (ALIGN - 1)) & ~ALIGN_MASK;
+  // calculate next_addr
+  uint32_t next_addr = ptr + size;
+
+  if(next_addr > (uint32_t)heap.end){
+    return NULL;
+  }
+
+  addr = next_addr;
+  return (void *)ptr;
 }
 
 void free(void *ptr) {
