2040281cdeccde4f6a31a72edcd56f6f378b9245
Letong Han <hanlt@shanghaitech.edu.cn>
2024-03-31 17:50:22 +0800

    [feat] fbdraw

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,12 +9,21 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR),
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *color = (uint32_t *)(ctl->pixels);
+  int w = inw(VGACTL_ADDR + 2);
+  for (int i = 0; i < ctl->w; ++i) {
+    for (int j = 0; j < ctl->h; ++j) {
+      outl(FB_ADDR + ((ctl->x + i) + (ctl->y + j) * w) * 4,
+           color[i + j * ctl->w]);
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
