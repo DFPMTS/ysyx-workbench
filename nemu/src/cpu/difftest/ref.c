bd886e774406a1aad826d3858d1d52db1288a1ba
Letong Han <hanlt@shanghaitech.edu.cn>
2024-04-08 16:29:12 +0800

    [feat] nemu as ref

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,16 +18,50 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
+#define NR_GPR MUXDEF(CONFIG_RVE,16,32)
+
+struct diff_context_t {
+  word_t gpr[NR_GPR];
+  word_t pc;
+  
+};
+
+static void difftest_set_regs(void *diff_context){
+  struct diff_context_t *context = diff_context;
+  for (int i = 0; i < NR_GPR; ++i) {
+    cpu.gpr[i] = context->gpr[i];
+  }
+  cpu.pc = context->pc;
+}
+
+static void difftest_get_regs(void *diff_context) {
+  struct diff_context_t *context = diff_context;
+  for (int i = 0; i < NR_GPR; ++i) {
+    context->gpr[i] = cpu.gpr[i];
+  }
+  context->pc = cpu.pc;
+}
+
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    for (int i = 0; i < n; ++i) {
+      paddr_write(addr + i, 1, ((uint8_t *)buf)[i]);
+    }
+  }else{
+    assert(0);
+  }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if(direction == DIFFTEST_TO_REF){
+    difftest_set_regs(dut);
+  }else{
+    difftest_get_regs(dut);
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
