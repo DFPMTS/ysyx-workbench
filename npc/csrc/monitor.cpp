28550ac4b1da3602e3e51bcd8e1f4178e2da04ca
Letong Han <hanlt@shanghaitech.edu.cn>
2024-04-27 13:02:44 +0800

    [feat] init NPC monitor

---
+++ npc/csrc/monitor.cpp
@@ -0,0 +1,3 @@
+#include "monitor.hpp"
+
+void init_monitor(int argc, char *argv[]);
---
+++ npc/include/monitor.hpp
@@ -0,0 +1,6 @@
+#ifndef MONITOR_HPP
+#define MONITOR_HPP
+
+void init_monitor(int argc, char *argv[]);
+
+#endif
