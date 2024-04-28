b548fd874f52463f0b98845539cf0a223eb2b41c
Letong Han <hanlt@shanghaitech.edu.cn>
2024-04-28 15:05:42 +0800

    [feat] update npc.mk for -l/-e

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -15,10 +15,14 @@ LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
+NPCFLAGS += $(IMAGE).bin
+NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt 
+NPCFLAGS += -e $(IMAGE).elf
+
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME) MAINARGS='$(IMAGE).bin' sim
+	$(MAKE) -C $(NPC_HOME) MAINARGS="$(NPCFLAGS)" sim
