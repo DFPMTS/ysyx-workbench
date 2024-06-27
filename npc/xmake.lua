aa07ac59d95941cc7f29c226a0cfdf103a1b9aeb
Letong Han <hanlt@shanghaitech.edu.cn>
2024-06-27 16:35:57 +0800

    [feat] xmake.lua

---
+++ npc/xmake.lua
@@ -0,0 +1,48 @@
+target("Vtop")
+  add_rules("verilator.binary")
+  set_toolchains("@verilator")
+  add_values("verilator.flags", "--trace")
+
+  add_cxxflags("-fPIE")
+  on_config(function (target) 
+    local llvm_cxxflags = os.iorun("llvm-config --cxxflags")
+    local llvm_ldflags = os.iorun("llvm-config --libs")
+    target:add("cxxflags",llvm_cxxflags,"-fPIE")
+    target:add("ldflags",llvm_ldflags)
+  end)
+
+
+  add_files("csrc/*.cpp")
+  add_files("csrc/*.cc")
+  add_files("vsrc/*.sv")
+  add_files("vsrc/*.v")  
+  add_includedirs("$(buildir)")
+  add_includedirs("include")
+  add_options("ITrace", "MTrace", "Waveform")
+  add_configfiles("csrc/config.h.in")
+
+
+target("chisel")  
+  set_kind("phony")
+  on_build(function (target) 
+    os.exec("make verilog")
+  end)
+
+option("ITrace")
+  add_deps("Trace")
+  set_description("Enable instruction trace")
+  set_configvar("ITRACE", true)
+  set_default(true)  
+  set_category("Trace")
+
+option("MTrace")
+  add_deps("Trace")
+  set_description("Enable memory trace")
+  set_configvar("MTRACE", true)
+  set_default(true)
+  set_category("Trace")
+
+option("Waveform")
+  set_description("Enable waveform generation")
+  set_configvar("WAVE", true)
+  set_default(false)
