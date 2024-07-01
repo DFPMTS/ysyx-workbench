bbf5c02bb36828c7257e1beda51204bef1a76381
Letong Han <hanlt@shanghaitech.edu.cn>
2024-07-01 15:08:53 +0800

    [feat] config UART in _trm_init

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -15,13 +15,25 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #endif
 static const char mainargs[] = MAINARGS;
 
-void putch(char ch) { 
+void putch(char ch) {
+  while (!(inb(UART_BASE + UART_LS) & UART_LS_TFE_FLAG))
+    ;
   outb(UART_BASE + UART_TX, ch); 
 }
 
 void halt(int code) {
   asm volatile("mv a0, %0; ebreak" : :"r"(code));
   while (1);
+}
+
+static void uart_init()
+{
+  int8_t LC_old = inb(UART_BASE + UART_LC);
+  // the LC_DL bits should be 0 after reset
+  outb(UART_BASE + UART_LC, LC_old | UART_LC_DL_FLAG);
+  outb(UART_BASE + UART_DL2, 0);
+  outb(UART_BASE + UART_DL1, 1);
+  outb(UART_BASE + UART_LC, LC_old);
 }
 
 void _trm_init() {
@@ -40,6 +52,8 @@ void _trm_init() {
     *bss = 0;
     bss ++;
   }
+
+  uart_init();
 
   int ret = main(mainargs);
   halt(ret);
--- abstract-machine/am/src/riscv/ysyxsoc/ysyxsoc.h
+++ abstract-machine/am/src/riscv/ysyxsoc/ysyxsoc.h
@@ -3,7 +3,14 @@
 
 #include "riscv/riscv.h"
 
-#define UART_BASE (0x10000000)
-#define UART_TX  (0x0)
+#define UART_BASE 0x10000000
+#define UART_TX  0
+#define UART_DL1 0
+#define UART_DL2 1
+#define UART_LC 3
+#define UART_LC_DL_FLAG (1 << 7)
+#define UART_LS 5
+#define UART_LS_TFE_FLAG (1 << 5)
+
 
 #endif
