10ab63a53dbff124ca80c070cf141cdf36bf0766
Letong Han <hanlt@shanghaitech.edu.cn>
2024-07-24 15:31:43 +0800

    [refactor] soc/npc signals

--- npc/include/cpu.hpp
+++ npc/include/cpu.hpp
@@ -14,40 +14,33 @@ extern VerilatedVcdC *vcd;
 #define SIM_T 10
 extern uint64_t eval_time;
 
-#define concat_temp(x, y) (x##y)
+#define CONCAT(x, y) concat_temp(x, y)
+#define concat_temp(x, y) x##y
 static uint32_t dummy = 0;
-#ifdef NPC
-#define REG(x)                                                                 \
-  (concat_temp(                                                                \
-      top->rootp->npc_top__DOT__npc__DOT__idu__DOT__regfile__DOT__regs_, x))
-#define PC (top->rootp->npc_top__DOT__npc__DOT__ifu__DOT__pc)
-#define INST (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__ctrlBuffer_inst)
-#define VALID (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__validBuffer)
-#define JUMP                                                                   \
-  (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__ctrlBuffer_fuType == 1 &&     \
-   top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__ctrlBuffer_fuOp == 0)
-#define RD (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__ctrlBuffer_rd)
-#define RS1 (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__ctrlBuffer_rs1)
-#define IMM (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__dataBuffer_imm)
-#define DNPC                                                                   \
-  (top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__dnpcBuffer_valid              \
-       ? top->rootp->npc_top__DOT__npc__DOT__wbu__DOT__dnpcBuffer_pc           \
-       : PC + 4)
 
+#define IFU CONCAT(CPU, __DOT__ifu__DOT__)
+#define IDU CONCAT(CPU, __DOT__idu__DOT__)
+#define REGFILE CONCAT(IDU, regfile__DOT__regs_)
+#define EXU CONCAT(CPU, __DOT__exu__DOT__)
+#define MEM CONCAT(CPU, __DOT__mem__DOT__)
+#define WBU CONCAT(CPU, __DOT__wbu__DOT__)
+
+#define REG(x) (CONCAT(REGFILE, x))
+#define PC CONCAT(IFU, pc)
+#define INST CONCAT(WBU, ctrlBuffer_inst)
+#define VALID CONCAT(WBU, validBuffer)
+#define JUMP                                                                   \
+  (CONCAT(WBU, ctrlBuffer_fuType) == 1 && CONCAT(WBU, ctrlBuffer_fuOp == 0))
+#define RD CONCAT(WBU, ctrlBuffer_rd)
+#define RS1 CONCAT(WBU, ctrlBuffer_rs1)
+#define IMM CONCAT(WBU, dataBuffer_imm)
+#define DNPC                                                                   \
+  (CONCAT(WBU, dnpcBuffer_valid) ? CONCAT(WBU, dnpcBuffer_pc) : PC + 4)
+
+#ifdef NPC
+#define CPU top->rootp->npc_top__DOT__npc
 #else
-#define REG(x)                                                                              \
-  (concat_temp(                                                                             \
-      top->rootp                                                                            \
-          ->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__idu__DOT__regfile__DOT__regs_, \
-      x))
-#define PC                                                                     \
-  (top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__ifu__DOT__pc)
-#define INST                                                                   \
-  (top->rootp                                                                  \
-       ->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__wbu__DOT__dbgInBuffer_inst)
-#define VALID                                                                  \
-  (top->rootp                                                                  \
-       ->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__wbu__DOT__validBuffer)
+#define CPU top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu
 #endif
 struct difftest_context_t {
   uint32_t gpr[16];
