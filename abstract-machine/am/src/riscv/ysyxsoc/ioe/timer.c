62e273ac0006167b0e353f415cd75c7f048a6ee3
Letong Han <hanlt@shanghaitech.edu.cn>
2024-07-30 09:30:03 +0800

    [feat] am ioe timer

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
@@ -1,10 +1,21 @@
 #include <am.h>
 #include "ysyxsoc.h"
 
+#define RTC_ADDR 0x02000000
+
+static uint64_t boot_time;
+
+static uint64_t read_rtc()
+{
+  return ((uint64_t)inl(RTC_ADDR + 4) << 32) + inl(RTC_ADDR);
+}
+
 void __am_timer_init() {
+  boot_time = read_rtc();
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+  uptime->us = read_rtc() - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
