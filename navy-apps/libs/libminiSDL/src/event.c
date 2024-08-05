9e20f0a3844cd3b525f19cd1646a6d30ad06e178
Letong Han <hanlt@shanghaitech.edu.cn>
2024-08-05 17:04:38 +0800

    [feat] (navy-apps) simple SDL_WaitEvent

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,12 +1,24 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
+#define ARRLEN(arr) (sizeof(arr) / sizeof(arr[0]))
 
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
+
+static int find_key(const char *s){
+  for (int i = 0; i < ARRLEN(keyname); ++i) {
+    if(strcmp(keyname[i], s) == 0){
+      return i;
+    }
+  }
+  return 0;
+}
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
@@ -17,6 +29,22 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  if(!event){
+    return 1;
+  }
+  char buf[64];
+  char op[10];
+  char key[20];
+  while(!NDL_PollEvent(buf,sizeof(buf)));
+  sscanf(buf, "%s %s", op, key);
+  if (strcmp(op, "kd") == 0) {
+    event->type = SDL_KEYDOWN;
+  } else if (strcmp(op, "ku") == 0) {
+    event->type = SDL_KEYUP;
+  } else {
+    assert(0);
+  }
+  event->key.keysym.sym = find_key(key);
   return 1;
 }
 
