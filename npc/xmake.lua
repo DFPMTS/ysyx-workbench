323864e03119999971947669b505ddeb1badc114
tracer-ysyx <tracer@ysyx.org>
2024-11-15 05:00:19 -0700

    > sim RTL ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:00:19 up 53 days,  4:24, 16 users,  load average: 1.61, 2.38, 4.71

--- npc/xmake.lua
+++ npc/xmake.lua
@@ -6,7 +6,7 @@ target("Vtop")
   on_config(function (target) 
     local llvm_cxxflags = os.iorun("llvm-config --cxxflags")
     local llvm_ldflags = os.iorun("llvm-config --libs")
-    target:add("cxxflags",llvm_cxxflags,"-fPIE")
+    target:add("cxxflags",llvm_cxxflags,"-fPIE", "-std=c++20")
     target:add("ldflags",llvm_ldflags)
     local sdl2_cxxflags = os.iorun("sdl2-config --cflags")
     local sdl2_ldflags = os.iorun("sdl2-config --libs") .. "-lSDL2_image -lSDL2_ttf"
