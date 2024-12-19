1d05ab0db555d543b1b4b401f761b043c9e224cc
tracer-ysyx <tracer@ysyx.org>
2024-12-19 07:36:29 -0700

    > generate verilog ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:36:29 up 87 days,  7:00, 12 users,  load average: 0.75, 0.69, 0.81

--- npc/Core/src/Rename.scala
+++ npc/Core/src/Rename.scala
@@ -17,45 +17,104 @@ class RenameIO extends CoreBundle {
 class Rename extends CoreModule {
   val io = IO(new RenameIO)
 
+  // * Main signals: renameUop
   val uop = Reg(Vec(ISSUE_WIDTH, new RenameUop))
   val uopNext = Wire(Vec(ISSUE_WIDTH, new RenameUop()))
   val uopValid = RegInit(VecInit(Seq.fill(ISSUE_WIDTH)(false.B)))
 
   // * Submodules
-  val renamingTable = new RenamingTable
-  val freeList = new FreeList
+  val renamingTable = Module(new RenamingTable)
+  val freeList = Module(new FreeList)
 
-  // * Decode -> FreeList
-  for (i <- 0 until ISSUE_WIDTH) {
-    freeList.io.IN_renameReqValid(i) := io.IN_decodeUop(i).valid && io.IN_decodeUop(i).bits.rd =/= 0.U
-  }
+  // * Dataflow
+
+  // ** Decode -> FreeList
+  val needPReg = io.IN_decodeUop.map(decodeUop => decodeUop.valid && decodeUop.bits.rd =/= 0.U)
   val renameStall = freeList.io.OUT_renameStall
-
-  // * Decode -> RenamingTable
   for (i <- 0 until ISSUE_WIDTH) {
+    // * Allocate PReg
+    freeList.io.IN_renameReqValid(i) := needPReg(i)
+  }  
+
+  // ** FreeList <- Commit
+  for (i <- 0 until COMMIT_WIDTH) {
+    freeList.io.IN_commitValid(i) := io.IN_commitUop(i).valid
+    freeList.io.IN_commitRd(i) := io.IN_commitUop(i).bits.rd
+    freeList.io.IN_commitPReg(i) := io.IN_commitUop(i).bits.prd
+    freeList.io.IN_commitPrevPReg(i) := renamingTable.io.OUT_commitPrevPReg(i)
+  }
+
+  // ** Decode -> RenamingTable
+  for (i <- 0 until ISSUE_WIDTH) {
+    // * Read
     renamingTable.io.IN_renameReadAReg(i) := VecInit(io.IN_decodeUop(i).bits.rs1, io.IN_decodeUop(i).bits.rs2)
-    renamingTable.io.IN_renameWriteValid(i) := !renameStall && io.IN_decodeUop(i).valid && io.IN_decodeUop(i).bits.rd =/= 0.U
+    // * Write
+    renamingTable.io.IN_renameWriteValid(i) := !renameStall && needPReg(i)
     renamingTable.io.IN_renameWriteAReg(i) := io.IN_decodeUop(i).bits.rd
     renamingTable.io.IN_renameWritePReg(i) := freeList.io.OUT_renamePReg(i)
   }
 
-  // * RenamingTable <- Commit
+  // ** RenamingTable <- Writeback
   for (i <- 0 until MACHINE_WIDTH) {
     renamingTable.io.IN_writebackValid(i) := io.IN_writebackUop(i).valid
     renamingTable.io.IN_writebackPReg(i) := io.IN_writebackUop(i).bits.prd
   }
 
-  // * RenamingTable <- Commit
+  // ** RenamingTable <- Commit
   for (i <- 0 until COMMIT_WIDTH) {
     renamingTable.io.IN_commitValid(i) := io.IN_commitUop(i).valid
     renamingTable.io.IN_commitAReg(i) := io.IN_commitUop(i).bits.rd
     renamingTable.io.IN_commitPReg(i) := io.IN_commitUop(i).bits.prd
   }
 
-  // * Output Logic
-  // ** Decode <- Rename
+  // ** uopNext generation
   for (i <- 0 until ISSUE_WIDTH) {
-    io.IN_decodeUop(i).ready := !renameStall
+    uopNext(i).prd := Mux(needPReg(i), freeList.io.OUT_renamePReg(i), 0.U)
+    uopNext(i).prs1 := renamingTable.io.OUT_renameReadPReg(i)(0)
+    uopNext(i).prs2 := renamingTable.io.OUT_renameReadPReg(i)(1)
+
+    uopNext(i).src1Ready := renamingTable.io.OUT_renameReadReady(i)(0)
+    uopNext(i).src2Ready := renamingTable.io.OUT_renameReadReady(i)(0)
+
+    uopNext(i).fuType := io.IN_decodeUop(i).bits.fuType
+    uopNext(i).opcode := io.IN_decodeUop(i).bits.opcode
+    
+    uopNext(i).imm := io.IN_decodeUop(i).bits.imm
+    uopNext(i).pc := io.IN_decodeUop(i).bits.pc    
+
+    uopNext(i).predTarget := io.IN_decodeUop(i).bits.predTarget
+    uopNext(i).compressed := io.IN_decodeUop(i).bits.compressed
   }
 
+  // * Control
+  val inValid = io.IN_decodeUop.map(_.valid)
+  val outReady = io.OUT_renameUop(0).ready
+  val inFire = io.IN_decodeUop.map(_.fire)
+  val inReady = (!uopValid.reduce(_ || _) || outReady) && !renameStall
+
+
+  // ** update uop
+  for (i <- 0 until ISSUE_WIDTH) {
+    uop(i) := Mux(inFire(i), uopNext(i), uop(i))
+  }
+
+  // ** update uopValid  
+  when (io.IN_flush) {
+    uopValid := VecInit(Seq.fill(ISSUE_WIDTH)(false.B))
+  }.elsewhen(!inReady) {
+    uopValid := inValid
+  }
+
+  // * Output
+
+  // ** Decode <- Rename
+  for (i <- 0 until ISSUE_WIDTH) {
+    io.IN_decodeUop(i).ready := inReady
+  }  
+
+  // ** Rename -> Issue
+  for (i <- 0 until ISSUE_WIDTH) {
+    io.OUT_renameUop(i).valid := uopValid(i)
+    io.OUT_renameUop(i).bits := uop(i)    
+  }
 }
