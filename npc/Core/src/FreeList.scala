d84b56096c801976f877044f6598424165d3e932
tracer-ysyx <tracer@ysyx.org>
2024-12-18 23:05:36 -0700

    > generate verilog ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:05:36 up 86 days, 22:29, 20 users,  load average: 4.91, 5.17, 5.32

--- npc/Core/src/FreeList.scala
+++ npc/Core/src/FreeList.scala
@@ -4,14 +4,14 @@ import utils._
 
 class FreeListIO extends CoreBundle {
   // * Allocate new PReg
-  val IN_renameValid = Flipped(Vec(ISSUE_WIDTH, Bool()))
+  val IN_renameReqValid = Flipped(Vec(ISSUE_WIDTH, Bool()))
   val OUT_renamePReg = Vec(ISSUE_WIDTH, UInt(PREG_IDX_W))
 
   // * Free PReg (Commit)
   val IN_commitValid = Flipped(Vec(COMMIT_WIDTH, Bool()))
+  val IN_commitRd = Flipped(Vec(COMMIT_WIDTH, UInt(5.W)))
   val IN_commitPReg = Flipped(Vec(COMMIT_WIDTH, UInt(PREG_IDX_W)))
   val IN_commitPrevPReg = Flipped(Vec(COMMIT_WIDTH, UInt(PREG_IDX_W)))
-  val IN_commitLastest = Flipped(Vec(COMMIT_WIDTH, Bool()))
 
   // * Reset
   val IN_flush = Input(Bool())
@@ -24,20 +24,24 @@ class FreeList extends CoreModule {
   val tailPtr = RegInit(RingBufferPtr(NUM_PREG - 1, 0.U, 0.U))
 
   // * Allocate new PReg
+  val headPtrNext = WireDefault(headPtr)
   for (i <- 0 until ISSUE_WIDTH) {
-    when (io.IN_renameValid(i)) {
-      io.OUT_renamePReg(i) := freeList(headPtr.index)
-      headPtr := headPtr + 1.U
+    when (io.IN_renameReqValid(i)) {
+      io.OUT_renamePReg(i) := freeList(headPtrNext.index)
+      headPtrNext := headPtrNext + 1.U
     } .otherwise {
       io.OUT_renamePReg(i) := ZERO
     }
   }
+  headPtr := headPtrNext
 
   // * Free PReg (Commit)
+  val tailPtrNext = WireDefault(tailPtr)
   for (i <- 0 until COMMIT_WIDTH) {
     when (io.IN_commitValid(i)) {
-      freeList(tailPtr.index) := io.IN_commitPReg(i)
-      tailPtr := tailPtr + 1.U
+      freeList(tailPtrNext.index) := io.IN_commitPReg(i)
+      tailPtrNext := tailPtrNext + 1.U
     }
   }
+  tailPtr := tailPtrNext
 }
