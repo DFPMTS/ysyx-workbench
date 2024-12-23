831ef1bbad744a3fac4d8b07f48a636ee4f31752
tracer-ysyx <tracer@ysyx.org>
2024-12-22 23:54:59 -0700

    > generate verilog ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:54:59 up 90 days, 23:18, 12 users,  load average: 1.46, 1.13, 0.75

--- npc/Core/src/Elaborate.scala
+++ npc/Core/src/Elaborate.scala
@@ -25,7 +25,7 @@ object Elaborate_npc extends App {
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
       "disallowLocalVariables",
-      // "disallowPackedArrays",
+      "disallowPackedArrays=false",
       // "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
