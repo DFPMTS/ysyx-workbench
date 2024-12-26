1cefb5c7c00b6dfd33a308634dd8b1e8ab0dfaa0
tracer-ysyx <tracer@ysyx.org>
2024-12-26 04:33:08 -0700

    > generate verilog ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:33:08 up 94 days,  3:56, 12 users,  load average: 1.18, 1.02, 1.07

--- npc/Core/src/Scheduler.scala
+++ npc/Core/src/Scheduler.scala
@@ -21,23 +21,23 @@ class Scheduler extends CoreModule {
     io.OUT_renameUop(i).bits := io.IN_renameUop(0)
   }
 
-  def isALU(uop: RenameUop) = uop.fuType === FuType.ALU || uop.fuType === FuType.BRU
-  def isLSU(uop: RenameUop) = uop.fuType === FuType.LSU
-  // * ALU
-  val aluValid = (0 until ISSUE_WIDTH).map(i => io.IN_issueQueueValid(i) && isALU(io.IN_renameUop(i)))
-  val aluValidIndex = PriorityEncoder(aluValid)
-  val hasAluValid = aluValid.reduce(_ || _)
+  def isPort0(uop: RenameUop) = uop.fuType === FuType.ALU || uop.fuType === FuType.BRU || uop.fuType === FuType.CSR
+  def isPort1(uop: RenameUop) = uop.fuType === FuType.LSU
+  // * Port 0: ALU/BRU/CSR
+  val port0Valid = (0 until ISSUE_WIDTH).map(i => io.IN_issueQueueValid(i) && isPort0(io.IN_renameUop(i)))
+  val port0ValidIndex = PriorityEncoder(port0Valid)
+  val hasPort0Valid = port0Valid.reduce(_ || _)
 
-  io.OUT_issueQueueReady(aluValidIndex) := io.OUT_renameUop(0).ready
-  io.OUT_renameUop(0).valid := io.IN_issueQueueValid(aluValidIndex) && hasAluValid
-  io.OUT_renameUop(0).bits := io.IN_renameUop(aluValidIndex)
+  io.OUT_issueQueueReady(port0ValidIndex) := io.OUT_renameUop(0).ready
+  io.OUT_renameUop(0).valid := io.IN_issueQueueValid(port0ValidIndex) && hasPort0Valid
+  io.OUT_renameUop(0).bits := io.IN_renameUop(port0ValidIndex)
 
-  // * LSU
-  val lsuValid = (0 until ISSUE_WIDTH).map(i => io.IN_issueQueueValid(i) && isLSU(io.IN_renameUop(i)))
-  val lsuValidIndex = PriorityEncoder(lsuValid)
-  val hasLsuValid = lsuValid.reduce(_ || _)
+  // * Port 1: LSU
+  val port1Valid = (0 until ISSUE_WIDTH).map(i => io.IN_issueQueueValid(i) && isPort1(io.IN_renameUop(i)))
+  val port1ValidIndex = PriorityEncoder(port1Valid)
+  val hasPort1Valid = port1Valid.reduce(_ || _)
 
-  io.OUT_issueQueueReady(lsuValidIndex) := io.OUT_renameUop(1).ready
-  io.OUT_renameUop(1).valid := io.IN_issueQueueValid(lsuValidIndex) && hasLsuValid
-  io.OUT_renameUop(1).bits := io.IN_renameUop(lsuValidIndex)
+  io.OUT_issueQueueReady(port1ValidIndex) := io.OUT_renameUop(1).ready
+  io.OUT_renameUop(1).valid := io.IN_issueQueueValid(port1ValidIndex) && hasPort1Valid
+  io.OUT_renameUop(1).bits := io.IN_renameUop(port1ValidIndex)
 }
