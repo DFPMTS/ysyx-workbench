58be023ec0529f5afa6d797f46df62f1f1077d79
tracer-ysyx <tracer@ysyx.org>
2024-12-27 06:58:35 -0700

    > compile NEMU ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:58:35 up 95 days,  6:22, 13 users,  load average: 0.79, 0.80, 1.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,6 +55,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
+  printf("exec_once\n");
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
