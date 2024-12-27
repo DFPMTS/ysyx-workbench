d1c88a6197292e17efd1c9170e695c2de353cad1
tracer-ysyx <tracer@ysyx.org>
2024-12-27 06:57:14 -0700

    > compile NEMU ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:57:14 up 95 days,  6:20, 13 users,  load average: 0.61, 0.81, 1.14

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -209,7 +209,6 @@ uint32_t current_inst;
 
 static int decode_exec(Decode *s) {
   uint32_t i = s->isa.inst.val;
-  printf("--------------------pc: 0x%x inst: 0x%x\n", s->pc, i);
   int rd  = 0;
   word_t src1 = 0, src2 = 0, imm = 0;
   s->dnpc = s->snpc;
@@ -399,6 +398,7 @@ static int decode_exec(Decode *s) {
 }
 
 int isa_exec_once(Decode *s) {
+  printf("--------------------pc: 0x%x inst: 0x%x\n", s->pc, s->isa.inst.val);
   cpu.trap = INTR_EMPTY;
   s->isa.inst.val = inst_fetch(&s->snpc, 4);  
   if(HAS_TRAP) {
