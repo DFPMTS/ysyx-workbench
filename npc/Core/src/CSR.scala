301b8962f60c69a3032496462c328104ea3124b7
tracer-ysyx <tracer@ysyx.org>
2024-12-27 21:28:28 -0700

    > generate verilog ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:28 up 95 days, 20:52, 13 users,  load average: 0.68, 0.76, 0.95

--- npc/Core/src/CSR.scala
+++ npc/Core/src/CSR.scala
@@ -4,12 +4,41 @@ import utils._
 import Config.XLEN
 
 object CSRList {
-  val mstatus   = 0x300.U
-  val mtvec     = 0x305.U
-  val mepc      = 0x341.U
-  val mcause    = 0x342.U
-  val mvendorid = 0xf11.U
-  val marchid   = 0xf12.U
+  val sstatus        = 0x100.U
+  val sie            = 0x104.U
+  val stvec          = 0x105.U
+  val scounteren     = 0x106.U
+
+  val sscratch       = 0x140.U
+  val sepc           = 0x141.U
+  val scause         = 0x142.U
+  val stval          = 0x143.U
+  val sip            = 0x144.U
+  val satp           = 0x180.U
+
+  val mstatus        = 0x300.U
+  val misa           = 0x301.U
+  val medeleg        = 0x302.U
+  val mideleg        = 0x303.U
+  val mie            = 0x304.U
+  val mtvec          = 0x305.U
+  val mcounteren     = 0x306.U
+  val menvcfg        = 0x30A.U
+  val mstatush       = 0x310.U
+  val menvcfgh       = 0x31A.U
+  val mscratch       = 0x340.U
+  val mepc           = 0x341.U
+  val mcause         = 0x342.U
+  val mtval          = 0x343.U
+  val mip            = 0x344.U
+
+  val time           = 0xC01.U
+  val timeh          = 0xC81.U
+
+  val mvendorid      = 0xF11.U
+  val marchid        = 0xF12.U
+  val mipid          = 0xF13.U
+  val mhartid        = 0xF14.U
 }
 
 class CSRIO extends CoreBundle {
