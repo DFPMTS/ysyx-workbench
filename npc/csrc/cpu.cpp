375d84843748af157e65f8fecc15aa460661c47c
tracer-ysyx <tracer@ysyx.org>
2025-01-02 04:54:09 -0700

    > sim RTL ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:54:09 up 101 days,  4:17, 17 users,  load average: 0.66, 0.80, 0.90

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -8,10 +8,10 @@ Vtop *top;
 VerilatedFstC *fst;
 uint64_t eval_time;
 
-static const char *regs[] = {
-    "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-};
+static const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
+                             "s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
+                             "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
+                             "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 static uint32_t *gprs[32];
 bool begin_wave = false;
