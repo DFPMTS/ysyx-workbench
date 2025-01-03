05459e6d06f200c57e4ba8b6303595e801c72333
tracer-ysyx <tracer@ysyx.org>
2025-01-02 17:42:19 -0700

    > sim RTL ysyx_23060238 Letong Han Linux epyc2 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:19 up 101 days, 17:06, 17 users,  load average: 2.01, 0.90, 0.52

--- npc/xmake.lua
+++ npc/xmake.lua
@@ -18,10 +18,10 @@ target("Vtop")
   add_files("csrc/*.cpp")
   add_files("csrc/*.cc")
   if get_config("sim_target") == "npc" then
+    add_values("verilator.flags", "--threads", "4")
     add_values("verilator.flags", "--trace-fst")
     add_values("verilator.flags", "-sv")
   else 
-    add_values("verilator.flags", "--threads", "4")
     add_values("verilator.flags", "--trace-fst", "--timescale", "1ns/1ps", "--no-timing", "--top-module", "ysyxSoCFull", "--autoflush", "-Wno-WIDTHEXPAND")
     add_values("verilator.flags","-I../ysyxSoC/perip/uart16550/rtl")
     add_values("verilator.flags","-I../ysyxSoC/perip/spi/rtl")
